3c40fae7c269ea0ddfdd506ea8f3a756f7574806
ext-frranier_meli <ext-frranier@mercadolibre.com>
2023-08-25 16:53:52 -0300

    securize this shiat

--- server/public/firebase.js
+++ server/public/firebase.js
@@ -1,22 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 
-// Add Firebase products that you want to use
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 
-// TODO: Replace the following with your app's Firebase project configuration
+const apiKey = process.env.API_KEY;
+const authDomain = process.env.AUTH_DOMAIN;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCYMDy_Rf1o82MmWVyI0p0fRwyJ2w2dcTs",
-  authDomain: "cadavre-exquis-9c7af.firebaseapp.com",
-  projectId: "cadavre-exquis-9c7af",
-  storageBucket: "cadavre-exquis-9c7af.appspot.com",
-  messagingSenderId: "950495760420",
-  appId: "1:950495760420:web:57784044dd9be8f95548b1",
-  measurementId: "G-80DH0CRH6R",
-};
+  apiKey,
+  authDomain,
+}
 
 const app = initializeApp(firebaseConfig);
 
@@ -27,7 +23,7 @@ function signIn(email, password) {
     .then((userCredential) => {
       const user = userCredential.user;
       document.cookie = `accessToken=${user.accessToken}`;
-      
+
       htmx.ajax("GET", '/home', '#main')
     })
     .catch((error) => {
