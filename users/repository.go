c68fa36783c68a75750c5678e27a0f839bf758e5
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2023-08-28 06:25:45 -0300

    cadavre-exquis v0.1.0

---
+++ users/repository.go
@@ -0,0 +1,47 @@
+package users
+
+import (
+	"cadavre-exquis/firebase/auth"
+	"cadavre-exquis/firebase/firestore"
+	"cadavre-exquis/models"
+)
+
+func createAuth(user_name string, email string, password string) (*auth.Auth, error) {
+	return auth.CreateAuth(user_name, email, password)
+}
+
+func saveUserToDB(newUser *models.User, uid string) (*models.User, error) {
+	dsnap, err := firestore.SetDoc("users", uid, newUser, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &models.User{}
+	dsnap.DataTo(user)
+
+	return user, nil
+}
+
+func addCEToUser(uid string, contributions models.CEs) (*models.User, error) {
+	dsnap, err := firestore.SetDoc("users", uid, map[string]interface{}{"ces": contributions}, firestore.MergeAll)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &models.User{}
+	dsnap.DataTo(user)
+
+	return user, nil
+}
+
+func getUserByUID(uid string) (*models.User, error) {
+	dsnap, err := firestore.GetDoc("users", uid)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &models.User{}
+	dsnap.DataTo(user)
+
+	return user, nil
+}
